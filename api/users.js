cee98ab5a1fcad8465554a4d6ab6b82d55857924
Wes Yancy <79019393+wesyancy@users.noreply.github.com>
2022-10-16 14:50:49 -0500

    formatting

--- api/users.js
+++ api/users.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const usersRouter = express.Router();
 const { getAllUsers, getUserByUsername, createUser } = require('../db')
-
 const jwt = require('jsonwebtoken');
 const token = jwt.sign({ id: 1, username: 'albert' }, process.env.JWT_SECRET);
 
 usersRouter.use((req, res, next) => {
+  
     console.log("A request is being made to /users");
     next();
 
 });
 
 usersRouter.get('/', async (req, res) => {
-    const users = await getAllUsers(); 
+    
+  const users = await getAllUsers(); 
     
     res.send({
         users
@@ -21,9 +22,11 @@ usersRouter.get('/', async (req, res) => {
 });
 
 usersRouter.post('/login', async (req, res, next) => {
+    
     const { username, password } = req.body;
   
     if (!username || !password) {
+      
       next({
         name: "MissingCredentialsError",
         message: "Please supply both a username and password"
@@ -32,12 +35,15 @@ usersRouter.post('/login', async (req, res, next) => {
     }
   
     try {
+      
       const user = await getUserByUsername(username);
   
       if (user && user.password == password) {
+        
         res.send({ message: "you're logged in!", token });
 
       } else {
+        
         next({ 
           name: 'IncorrectCredentialsError', 
           message: 'Username or password is incorrect'
@@ -45,6 +51,7 @@ usersRouter.post('/login', async (req, res, next) => {
 
       }
     } catch(error) {
+      
       console.log(error);
       next(error);
 
@@ -53,12 +60,15 @@ usersRouter.post('/login', async (req, res, next) => {
   });
 
   usersRouter.post('/register', async (req, res, next) => {
+    
     const { username, password, name, location } = req.body;
   
     try {
+      
       const _user = await getUserByUsername(username);
   
       if (_user) {
+        
         next({
           name: 'UserExistsError',
           message: 'A user by that username already exists'
@@ -72,18 +82,19 @@ usersRouter.post('/login', async (req, res, next) => {
         location,
       });
   
-      const token = jwt.sign({ 
+      const token = jwt.sign({         
         id: user.id, 
         username
       }, process.env.JWT_SECRET, {
         expiresIn: '1w'
       });
   
-      res.send({ 
+      res.send({        
         message: "thank you for signing up",
         token 
       });
     } catch ({ name, message }) {
+      
       next({ name, message })
     } 
   });
